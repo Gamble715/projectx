536f4d3f1c9e55ce034b54eea59660f123e515b5
Daniel McAdam <Daniel.gamble.mcadam@gmail.com>
2013-03-25 17:32:23 -0400

    Add Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,14 @@ Projectx::Application.configure do
 
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Paperclip should use S3 on Heroku
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => "AKIAIHLTWJ6GHXTGHY5A",
+      :secret_access_key => "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"
+    }
+  }
 end
